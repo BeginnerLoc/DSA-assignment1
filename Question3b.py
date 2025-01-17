6f3d6c9eba3d4a93cdfd1904ac21619ac33c7da7
ngtrolling <ngtrolling1208@gmail.com>
2025-01-17 17:07:50 +0800

    Updated 3b

--- Question3b.py
+++ Question3b.py
@@ -1,1 +1,37 @@
-# Kai jie
+# Stable Sorting Algorithm - Merge Sort
+
+
+
+
+
+# -----------------------------------------------------------------------------
+# Unstable Sorting Algorithm - Quick Sort
+# -----------------------------------------------------------------------------
+
+Arr = [8,2,4,6,7,8,5]
+
+def QuickSort(inputArr) :
+
+    # Array <= 1 element means already sorted
+    if len(inputArr) <= 1: 
+        return inputArr
+
+    # Choose a pivot, in this case, we use the last value in the array
+    pivot = inputArr[-1]
+    less_than_pivot, equal_to_pivot, greater_than_pivot = [], [], []
+    
+    # Iterate through all the elements in the array
+    for x in inputArr:
+        # Values less than pivot in one array. Values more than pivot in another array
+        if x < pivot:
+            less_than_pivot.append(x)
+        elif x > pivot:
+            greater_than_pivot.append(x)
+        else:
+            equal_to_pivot.append(x)
+
+    #  Recursively sort the sub arrays and combine the results
+    return QuickSort(less_than_pivot) + equal_to_pivot + QuickSort(greater_than_pivot)
+
+print(QuickSort(Arr))
+
